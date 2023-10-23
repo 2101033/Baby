b242ed7d786bcda2dd5bf40fbaed3fdcf0c23575
asdfqwer0910 <115201468+asdfqwer0910@users.noreply.github.com>
2023-10-23 14:14:47 +0900

    コントローラー追記

--- src/main/java/com/example/demo/controller/BabyController.java
+++ src/main/java/com/example/demo/controller/BabyController.java
@@ -1,8 +1,18 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BabyController {
-
+	
+	@GetMapping("login")
+	public String loginView() {
+		return "login";
+	}
+	
+	@GetMapping("weight")
+	public String weightView() {
+		return "weight";
+	}
 }
