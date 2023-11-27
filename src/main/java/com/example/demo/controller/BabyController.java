1ab224d9ef88b7d56e622d44d5fb243dd8b988ac
kenta1226 <115599321+kenta1226@users.noreply.github.com>
2023-11-27 14:39:34 +0900

    kenta
    HTML: invitation
    　　    logout
                profile
    CSS: invitation
            logout
            profile
    残りは次回！

--- src/main/java/com/example/demo/controller/BabyController.java
+++ src/main/java/com/example/demo/controller/BabyController.java
@@ -1,8 +1,23 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BabyController {
 
+	@GetMapping("profile")
+	public String profile() {
+		return "Profile";
+	}
+	
+	@GetMapping("invitation")
+	public String invitation() {
+		return"Invitation";
+	}
+
+	@GetMapping("logout")
+	public String logout() {
+		return"logout";
+	}
 }
